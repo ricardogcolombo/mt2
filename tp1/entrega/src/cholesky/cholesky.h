f77fd4567989a6440bdf10bbd09fc16e08107af0
ricardogcolombo <ricardogcolombo@gmail.com>
2016-04-26 00:30:32 -0300

    Agrego carpeta tp2 y muevo todo a la carpeta tp1

---
+++ tp1/entrega/src/cholesky/cholesky.h
@@ -0,0 +1,7 @@
+#ifndef __CHOLESKY_H_INCLUDED__   //   #define this so the compiler knows it has been included
+#define __CHOLESKY_H_INCLUDED__   //   #define this so the compiler knows it has been included
+#include "../matriz/matriz.h"
+
+double* cholesky(Matriz* m ,double *b);
+
+#endif
