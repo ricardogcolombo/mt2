67119fcdcbfe466cb0ca0605acf399e1ab097987
ricardogcolombo <ricardogcolombo@gmail.com>
2016-04-29 06:51:19 -0300

    agrego tp2

---
+++ tp2/src/main.cpp
@@ -0,0 +1,22 @@
+//Include all our files here
+#include "matriz/matriz.h"
+
+//standard
+/* #include <algorithm>    // std::sort */
+// #include <cmath>
+// #include <climits>
+// #include <vector>       // std::vector
+// #include <iostream>
+// #include <fstream>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sstream>
+// #include <fstream>
+// #include <string.h>
+// #include <sys/time.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+}
