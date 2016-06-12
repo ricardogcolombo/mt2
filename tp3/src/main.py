5b42d8e48dc28e53c8378dcc4ba39f789977f88b
ricardogcolombo <ricardogcolombo@gmail.com>
2016-06-12 02:16:36 -0300

    tp3 codigo para armar la primera prediccion

--- tp3/src/main.py
+++ tp3/src/main.py
@@ -1,39 +1,61 @@
+#!/usr/bin/python
 import csv
 import numpy as np
+import matplotlib as mp
+import matplotlib.pyplot as plt
+import math
 
 
-
-def getItemsForAirport(csvName,airport,mes):
+def getItemsForAirport(anioInicial,cantAnios,airport,mes,cantMesesTrain,mesesTrain):
     #  leo el csv
-    reader=csv.reader(open(csvName,"rb"),delimiter=',')
+    result= []
+    filename = anioInicial
+    for i in range (0,cantAnios+1):
+	print "Abriendo base " + str(filename) + ".."
+	ifile  = open(str(filename)+".csv", "rb")
+	reader = csv.reader(ifile)
 
-    #  creo un array con los elementos con numpy
-    x=list(reader)
-    result=np.array(x)
+	for row in reader:
+            if row[15]!="NA" and row[16]==airport and row[25]!="NA" and int(row[15])>15 and row[16]==airport and int(row[25])>0:
+                result.append(list(row))
 
-    jfkfiltered = []
-    clefiltered = np.array(1)
+	ifile.close()
+	filename+=1
+
+    print "termino de leer"
+    countAnios= 0
+    anioActual= int(result[0][0])
     for item in result:
-        if item[15]!="NA" and item[16]==airport and item[25]!="NA":
-            if int(item[15])>15 and item[16]==airport and int(item[25])>0:
-                jfkfiltered.append(item)
-        #  elif result[n][15]>15 & result[n][16]=="CLE" & result[n][26]>0:
-            #  jfkfiltered.push(item)
+        if anioActual < int(item[0]):
+            countAnios+=1
+            anioActual = int(item[0])
+        mes[int(item[1])+(12*countAnios)-1]+=1
 
-    #  semana= np.zeros(5, dtype=np.int)
-
-    for item in jfkfiltered:
-        mes[int(item[1])-1]+=1
-        np.append(mes,1)
-        #  semana[item[2]-1]+=1
-
-
-
-    return
+    x = []
+    for i in range(0, cantMesesTrain):
+        mesesTrain[i]=mes[i]
+        x.append(math.sin(i+1))
+        
+    A = np.vstack([x, np.ones(len(x))]).T
+    m, c = np.linalg.lstsq(A, mesesTrain)[0]
+    return m , c
 
 #  print jfkfiltered
+#  creo el array con todos los meses que voy a graficar
+cantAnios = 1
+cantMesesTrain = 12
+mesesTrain = np.zeros(cantMesesTrain, dtype=np.int)
+mes = np.zeros((cantAnios+1)*12, dtype=np.int)
+m,c = getItemsForAirport(2007,1,"JFK",mes,cantMesesTrain,mesesTrain)
+ejeXMeses= []
+for i in range(1,(cantAnios+1)*12+1):
+    ejeXMeses.append(i)
 
-mes = np.zeros(12, dtype=np.int)
-getItemsForAirport("train.csv","JFK",mes)
+plt.plot(ejeXMeses,mes)
+for i in range(0,12):
+    plt.plot(i+1,mesesTrain[i],'bs')
 
-print mes
+for i in range(12,24):
+    plt.plot(i+1,m*math.sin(i)+c,'bs')
+
+plt.show()
