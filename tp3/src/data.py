8b5cb2642d7f8adceccab3731ff8ef0b6a8d6150
John Doe <johndoe@example.com>
2016-06-17 18:11:21 -0300

    filtros por empresa y aeropuerto

--- tp3/src/data.py
+++ tp3/src/data.py
@@ -1,12 +1,17 @@
-import fileManagment
+import fileManagment as fm
+import filterEmpresa as fe
+from filterEmpresa import *
 import numpy
+import csv
 
 anio =1987
-cant = 21
+cant = 1
 final = anio+cant
 airport = "JFK"
+empresa = "AA"
 for i in range(0,cant+1):
-    data = fileManagment.procesarData(anio,0,airport)
-    print "termine numero "+ str(i)
-    numpy.savetxt(str(anio)+'.csv', data, fmt="%d" ,delimiter=",")
+    print "Procesando archivo: "+ str(anio)
+    ofile  = str(anio)+ '-' + str(empresa) + '.csv'
+    data = fe.getDataByEmpresa(anio,empresa,airport)
+    numpy.savetxt(ofile, data, fmt="%d" ,delimiter=",")
     anio +=1
---
+++ tp3/src/filterEmpresa.py
@@ -0,0 +1,21 @@
+import csv
+import numpy as np
+
+def getDataByEmpresa(anioInicial,empresa,airport):
+    result= []
+    filename = anioInicial
+    ifile  = open(str(filename)+".csv", "rb")
+    reader = csv.reader(ifile)
+
+    for row in reader:
+	if row[8]==empresa and row[16]==airport:
+		result.append(list(row))
+    ifile.close()
+ 
+    datosFiles= np.zeros(12, dtype=np.int)
+    for item in result:
+        datosFiles[int(item[1])-1]+=1
+
+    return datosFiles
+
+
