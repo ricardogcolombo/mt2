60dedbe8695721c4a48037b3638cc7c9466e3bef
ricardogcolombo <ricardogcolombo@gmail.com>
2016-07-01 23:59:34 -0300

    arreglos

--- tp1/entrega/src/cholesky/cholesky.h
+++
@@ -1,7 +0,0 @@
-#ifndef __CHOLESKY_H_INCLUDED__   //   #define this so the compiler knows it has been included
-#define __CHOLESKY_H_INCLUDED__   //   #define this so the compiler knows it has been included
-#include "../matriz/matriz.h"
-
-double* cholesky(Matriz* m ,double *b);
-
-#endif
