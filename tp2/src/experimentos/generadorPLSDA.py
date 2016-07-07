d523d548bf9f90e6deca5d50d8f309f74a0d7e82
sov4nt4 <sov4nt4@debian>
2016-07-07 01:36:43 -0300

    agrego cosas de plsda

--- tp2/src/experimentos/generadorPLSDA.py
+++ tp2/src/experimentos/generadorPLSDA.py
@@ -8,30 +8,32 @@ print("GENERANDO CASOS DE TEST...")
 
 
 k = 30
-alfa =10 
+alfa =15
 gamma = 50
 K = 10
 totalTest = 4200
-for j in range(1,alfa+1):
-    fo = open("pca/test"+str(j)+".in", "wb")
+for n in range (1,15):
+	for j in range(1,alfa+1):
 
-    print "Generando archivo " + str(j)
-    fo.write("./data/" + " " + str(k) + " " + str(alfa)+ " " + str(j)+ " " + str(K))
-    fo.write("\n")
-    for i in range(0,K):
-        actual = 0
-        salida =np.ones((42000,), dtype=np.int)
-        while actual<totalTest :
-            pos = randint(0,41999)
-            if(salida[pos]==1):
-                salida[pos]=0
-                actual+=1
+	    fo = open("plsda/plsda"+str(n)+"/test"+str(j)+".in", "wb")
 
-        for i in range(0,42000):
-            if(i==41999):
-                fo.write(str(salida[i]))
-            else:
-                fo.write(str(salida[i])+ " ")
+	    print "Generando archivo " + str(j)
+	    fo.write("./data/" + " " + str(n) + " " + str(alfa)+ " " + str(j)+ " " + str(K))
+	    fo.write("\n")
+	    for i in range(0,K):
+		actual = 0
+		salida =np.ones((42000,), dtype=np.int)
+		while actual<totalTest :
+		    pos = randint(0,41999)
+		    if(salida[pos]==1):
+			salida[pos]=0
+			actual+=1
 
-        fo.write("\n")
-    fo.close()
+		for i in range(0,42000):
+		    if(i==41999):
+			fo.write(str(salida[i]))
+		    else:
+			fo.write(str(salida[i])+ " ")
+
+		fo.write("\n")
+	    fo.close()
